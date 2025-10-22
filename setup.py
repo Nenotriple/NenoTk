210716eb284de7e9cf79b6c51eccc02bbdc7c8f2
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2025-10-22 06:37:20 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    with open(filename, encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+setup(
+    name="nenotk",
+    version="0.1.0",
+    description="A collection of custom Tkinter widgets and utilities for Python 3.10+.",
+    author="Nenotriple",
+    url="https://github.com/Nenotriple/NenoTk",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=parse_requirements("requirements.txt"),
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
