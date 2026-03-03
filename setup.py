de3b06d12249a5df38c48cc24599d465d8c6d2fc
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2026-03-03 15:03:31 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
 
 setup(
     name="nenotk",
-    version="0.1.2",
+    version="0.1.3",
     description="A collection of custom Tkinter widgets and utilities for Python 3.10+.",
     author="Nenotriple",
     url="https://github.com/Nenotriple/NenoTk",
